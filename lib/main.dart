f0ece8ce499b68267138f5368d41108e27bf4309
Lakindu Widuranga Alwis <vidurangalakindu@gmail.com>
2022-12-18 22:57:07 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+//the main function is the starting point for all our flutter apps
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+         appBar: AppBar(
+          title: Text('I Am Rich'), //app bar title
+           backgroundColor: Colors.black, //app bar background color
+         ),
+      )
+    ));
